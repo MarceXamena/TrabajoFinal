03a9ae40b9e36d61394f2153312008a12bed7c52
MarceXamena <marceloxamena@gmail.com>
2023-09-24 20:26:06 -0300

    definiendo métodos para Usuario

--- APP/models.py
+++ APP/models.py
@@ -14,3 +14,18 @@ class Usuario:
         params = (Usuario.ID_usuario, Usuario.nombre, Usuario.correo_electronico, Usuario.contrasena, Usuario.foto_perfil)
         DatabaseConnection.execute_query(query, params)
     
+    @classmethod
+    def get_Usuario(Usuario):
+        query = 'SELECT ID_usuario, nombre, correo_electronico, contrasena, foto_perfil FROM db_sqadchat.usuarios WHERE ID_usuario = %s'
+        params = Usuario.ID_usuario
+        result = DatabaseConnection.fetch_one(query, params)
+        if result is not None:
+            return Usuario(
+                ID_usuario = Usuario.ID_usuario
+                nombre = Usuario.nombre
+                correo_electronico = Usuario.correo_electronico
+                contrasena = Usuario.contrasena
+                foto_perfil = Usuario.foto_perfil
+            )
+        else:
+            return None
